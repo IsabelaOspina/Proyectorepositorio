509c2f6ad4b32d622759718df50eeae83329cc2e
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2024-05-24 20:48:14 -0500

    cambios

--- parqueaderofinal/src/main/java/co/edu/uniquindio/poo/Parqueadero.java
+++ parqueaderofinal/src/main/java/co/edu/uniquindio/poo/Parqueadero.java
@@ -1,69 +1,14 @@
 package co.edu.uniquindio.poo;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Parqueadero {
     private List<Registro> registros;
 
-    public Parqueadero() {
-        this.registros = new ArrayList<>();
-    }
-
-    // Método para registrar la entrada de un vehículo
-    public void registrarEntrada(Registro registro) {
-        registros.add(registro);
-    }
-
-    // Método para registrar la salida de un vehículo
-    public void registrarSalida(Registro registro) {
-        registros.remove(registro);
-    }
-
-    // Método para generar el reporte diario del dinero recaudado
-    public double[] generarReporteDiario(LocalDate fecha) {
-        double totalCarro = 0;
-        double totalMotoClasica = 0;
-        double totalMotoHibrida = 0;
-
-        for (Registro registro : registros) {
-            if (registro.getHoraEntrada().toLocalDate().equals(fecha)) {
-                TipoVehiculo tipoVehiculo = registro.getTipoVehiculo();
-                double tarifa = calcularTarifa(registro, tipoVehiculo);
-
-                // Actualizar el total recaudado según el tipo de vehículo
-                switch (tipoVehiculo) {
-                    case CARRO:
-                        totalCarro += tarifa;
-                        break;
-                    case MOTOCLASICA:
-                        totalMotoClasica += tarifa;
-                        break;
-                    case MOTOHIBRIDA:
-                        totalMotoHibrida += tarifa;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-
-        // Devolver un array con los totales recaudados para cada tipo de vehículo
-        return new double[] { totalCarro, totalMotoClasica, totalMotoHibrida };
-    }
-
-    // Método para generar el reporte mensual del dinero recaudado
-    public double generarReporteMensual(int mes) {
-        double totalRecaudado = 0;
-        for (Registro registro : registros) {
-            if (registro.getHoraEntrada().getMonthValue() == mes) {
-                totalRecaudado += calcularTarifa(registro, registro.getTipoVehiculo());
-            }
-        }
-        return totalRecaudado;
-    }
-
+    /**
+     * metodo para calcular la tarifa de los vehiculos
+     * @param registro
+     * @param tipoVehiculo
+     * @return
+     */
     public double calcularTarifa(Registro registro, TipoVehiculo tipoVehiculo) {
         long horas = registro.CalcularHoraTotal(registro.getHoraEntrada(), registro.getHoraSalida());
         double tarifa;
@@ -83,9 +28,5 @@ public class Parqueadero {
         }
 
         return horas * tarifa;
-    }
-
-    public void setRegistros(List<Registro> registros) {
-        this.registros = registros;
     }
 }
