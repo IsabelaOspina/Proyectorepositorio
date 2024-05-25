9bc128933db4c76711fe51d64674d99a61b95304
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2024-05-24 21:47:32 -0500

    se creo la clase puesto

---
+++ parqueaderofinal/src/main/java/co/edu/uniquindio/poo/Puesto.java
@@ -0,0 +1,50 @@
+package co.edu.uniquindio.poo;
+
+public class Puesto {
+    private boolean disponible;
+
+    /**
+     * constructo  clase puesto
+     * @param disponible
+     */
+    public Puesto(boolean disponible) {
+        this.disponible=true;
+    }
+
+    /**
+     * metodo para cambiar el puesto de estado
+     */
+    public void cambiarEstado(){
+        if(this.disponible){
+            this.disponible=false;
+        
+        }else{
+            this.disponible=true;
+        }
+
+    }
+
+    public void identificarPropietario(Vehiculo vehiculo){
+        vehiculo.getPropietario();
+
+    }
+
+
+    /**
+     * metodo get
+     * @return
+     */
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    /**
+     * metodo sed
+     */
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+   
+    
+}
